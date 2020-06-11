f3e7fae8bc94dc3d229ab687986f0ec404743d33
Artem Ilinykh <devsinglesly@gmail.com>
2020-06-11 23:29:19 +0600

    save method implemented

--- src/repository/StageRepository.ts
+++ src/repository/StageRepository.ts
@@ -5,11 +5,39 @@
  */
 import { Stage } from "../entity/Stage";
 import { Injectable, NotImplementedException } from "@nestjs/common";
+import { Collection, Connection } from "mongoose";
+import { InjectConnection } from "@nestjs/mongoose";
+
+/*
+
+    {
+        name:
+        order:
+        changeName()
+    }
+
+    { name, order }
+
+ */
 
 @Injectable()
 export class StageRepository {
+
+    private readonly collection: Collection = this.connection.collection("stages");
+
+    constructor(
+        @InjectConnection() private readonly connection: Connection
+    ) {
+    }
+
     public async save(stage: Stage): Promise<void> {
-        throw new NotImplementedException();
+        const document = {
+            name: Reflect.get(stage, "name"),
+            order: Reflect.get(stage, "order")
+        };
+
+        const result = await this.collection.insertOne(document);
+        Reflect.set(stage, "id", result.insertedId.toString());
     }
 
     public async update(stage: Stage): Promise<void> {
